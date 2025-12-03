1050d9dd34b4ee126e83490122b86cdf5945cc01
Priyanka <priyankamaihra123456@gmail.com>
2025-12-03 11:12:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,15 +11,17 @@ import AddOnsSection from "./Components/AddOnsSection/AddOnsSection.jsx";
 import FeatureGridSection from "./Components/FeatureGridSection/FeatureGridSection.jsx";
 import LicensedSection from "./Components/LicensedSection/LicensedSection.jsx";
 import Footer from "./Components/Footer/Footer.jsx";
+
 import LoginPage from "./pages/LoginPage.jsx";
 import Signup from "./pages/Signup/Signup.jsx";
-
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />    {/* <-- GLOBAL (every page) */}
+      
       <Routes>
 
         {/* HOME PAGE */}
@@ -27,7 +29,6 @@ function App() {
           path="/"
           element={
             <>
-              <Navbar />
               <HeroSection />
               <NextSection />
               <IndustriesSection />
@@ -38,20 +39,28 @@ function App() {
               <AddOnsSection />
               <FeatureGridSection />
               <LicensedSection />
-              <Footer />
             </>
           }
         />
 
         {/* LOGIN PAGE */}
-        <Route path="/login" element={<LoginPage />} />
+        <Route 
+          path="/login" 
+          element={<LoginPage />} 
+        />
 
         {/* SIGNUP PAGE */}
-        <Route path="/signup" element={<Signup />} />
+        <Route 
+          path="/signup" 
+          element={<Signup />} 
+        />
 
       </Routes>
+
+      <Footer />   {/* <-- GLOBAL Footer */}
     </BrowserRouter>
   );
 }
 
 export default App;
+
